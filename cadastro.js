c0346ee7bddad350d8c89dd2aee21fc050a2dd5c
Montalvas <62609007+montalvas@users.noreply.github.com>
2020-09-27 20:58:06 -0300

    Update

--- cadastro.js
+++ cadastro.js
@@ -1,7 +1,26 @@
 window.onload = () => {
-    document.getElementById('enviar').addEventListener('click', () => {
-        let cat = document.getElementById('categoria');
+    let cat = document.getElementById('categoria');
+    let grau = document.getElementById('grau');
+    cat.addEventListener('change', () => {
+        grau.innerHTML = '';
+        let i = 1;
         let categoria = cat.options[cat.selectedIndex].text;
-        console.log(categoria);
+        if (categoria === 'Fundamental') {
+            for (i; i <= 8; i++) {
+                grau.innerHTML += `<option>${i}ª Série</option>`;
+            }
+        } else {
+            for (i; i <= 3; i++) {
+                grau.innerHTML += `<option>${i}º Ano</option>`;
+            }
+        }
+    })
+
+
+    document.getElementById('enviar').addEventListener('click', () => {
+        let codAluno = document.getElementById('codAluno').value;
+        let nomeAluno = document.getElementById('nomeAluno').value;
+        let serie = grau.options[grau.selectedIndex].text;
+        console.log(codAluno, nomeAluno, serie);
     })
 }
